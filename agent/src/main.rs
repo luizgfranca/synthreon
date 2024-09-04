6351274cf62a6d1d30f5c1b7769d881a7fb76d7a
luiz <luizgfc@proton.me>
2024-09-04 07:21:01 -0300

    agent: very basic heartbeat events (not networking for now)

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,3 +1,15 @@
+use std::{thread, time};
+
 fn main() {
-    println!("Hello, world!");
+    let heartbeat_interval = time::Duration::from_millis(1000);
+
+    loop {
+        let now = time::SystemTime::now()
+            .duration_since(time::UNIX_EPOCH)
+            .expect("SystemTimeError comparing current time and unix timestamp")
+            .as_millis();
+
+        println!("PING {now}");
+        thread::sleep(heartbeat_interval);
+    }
 }
