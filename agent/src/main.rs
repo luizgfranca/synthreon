100e3bb0649240b55afa4a8547dfc9d8c62d4fdd
luiz <luizgfc@proton.me>
2024-09-06 00:57:38 -0300

    agent: basic heartbeat transmission implementation

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,5 +1,26 @@
 use std::{io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}, thread, time};
 
+static HEARTBEAT_INTERVAL: std::time::Duration = time::Duration::from_millis(1000);
+
+enum Command {
+    UNKNOWN,
+    CONNECT,
+    CLOSE
+}
+
+impl Command {
+    pub fn from(message: &str) -> Command {
+        if message.is_empty() {
+            Command::CLOSE
+        } else {
+            match message {
+                "connect" => Command::CONNECT,
+                _ => Command::UNKNOWN
+            }
+        }
+    }
+}
+
 fn wait_for_message(stream: &TcpStream) -> String {
     let reader = BufReader::new(stream);
             
@@ -17,33 +38,39 @@ fn wait_for_message(stream: &TcpStream) -> String {
     message.to_string()
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    println!("new connection");
-    
+fn transmit_heartbeat(mut stream: TcpStream) {
     loop {
-        let message = wait_for_message(&stream);
-        if message.is_empty() {
-            break;
-        }
-
-        println!("{message}");
-        stream.write_all(message.as_bytes()).expect("error responding");
-    }
-}
-
-fn main() {
-    let heartbeat_interval = time::Duration::from_millis(1000);
-
-    // loop {
-        let now = time::SystemTime::now()
+        let current_timestamp = time::SystemTime::now()
             .duration_since(time::UNIX_EPOCH)
             .expect("SystemTimeError comparing current time and unix timestamp")
             .as_millis();
 
-        println!("PING {now}");
-        thread::sleep(heartbeat_interval);
-    // }
+        let mut message = "PING ".to_string();
+        message.push_str(&current_timestamp.to_string());
+        message.push_str("\n");
+        print!("{message}");
 
+        let result = stream.write_all(message.as_bytes());
+        if result.is_err() {
+            println!("error sending heartbeat\n");
+            break;
+        }
+
+        thread::sleep(HEARTBEAT_INTERVAL);
+    }
+}
+
+fn handle_connection(stream: TcpStream) {
+    println!("new connection");
+    let command = Command::from(&wait_for_message(&stream));
+    match command {
+        Command::CONNECT => transmit_heartbeat(stream),
+        Command::UNKNOWN => println!("unknown command received"),
+        Command::CLOSE => println!("close connection"),
+    }
+}
+
+fn main() {
     let address = "127.0.0.1:25250";
     let listener = TcpListener::bind(address)
         .expect("failed to attach to {address}");
