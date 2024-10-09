d6c937dc5365cdec18821c63530e5c6557004d6d
luiz <luizgfc@proton.me>
2024-10-09 19:14:08 -0300

    control-panel: datasource generalization

--- control-panel/web/src/datasource/project.datasource.ts
+++ control-panel/web/src/datasource/project.datasource.ts
@@ -1,38 +1,8 @@
+import { Datasource } from "@/lib/datasource";
 import ProjectService, { ProjectDto } from "@/service/project.service";
 
-type LoadingStatus = 'none' | 'pending' | 'success' | 'fail';
-
-export class ProjectDatasource {
-    #loadingStatus: LoadingStatus = 'none';
-    #loadingPromise?: Promise<ProjectDto[]>;
-    
-    #data?: ProjectDto[];
-    #error: unknown;
-    
+export class ProjectDatasource extends Datasource<ProjectDto[]> {
     async fetch(): Promise<ProjectDto[]>{
         return ProjectService.queryProjects;
-    }
-
-    get(): ProjectDto[] {
-        switch(this.#loadingStatus) {
-            case 'none':
-                this.#loadingPromise = this.fetch();
-                this.#loadingPromise.
-                    then((data) => {
-                        this.#data = data;
-                        this.#loadingStatus = 'success'
-                    }).catch(e => {
-                        this.#error = e;
-                        this.#loadingStatus = 'fail'
-                    });
-                this.#loadingStatus = 'pending'
-                throw this.#loadingPromise;
-            case 'pending':
-                throw this.#loadingPromise;
-            case 'fail':
-                throw this.#error
-            case 'success':
-                return this.#data as ProjectDto[];
-        }
     }
 }
--- control-panel/web/src/lib/datasource.ts
+++ control-panel/web/src/lib/datasource.ts
@@ -1,3 +1,36 @@
-class Datasource<T> {
+type LoadingStatus = 'none' | 'pending' | 'success' | 'fail';
+
+export class Datasource<T> {
+    #loadingStatus: LoadingStatus = 'none';
+    #loadingPromise?: Promise<T>;
     
+    #data?: T;
+    #error: unknown;
+
+    async fetch(): Promise<T> {
+        throw new Error('fetch() should be overriden with the function to load your dataset')
+    }
+
+    get(): T {
+        switch(this.#loadingStatus) {
+            case 'none':
+                this.#loadingPromise = this.fetch();
+                this.#loadingPromise.
+                    then((data) => {
+                        this.#data = data;
+                        this.#loadingStatus = 'success'
+                    }).catch(e => {
+                        this.#error = e;
+                        this.#loadingStatus = 'fail'
+                    });
+                this.#loadingStatus = 'pending'
+                throw this.#loadingPromise;
+            case 'pending':
+                throw this.#loadingPromise;
+            case 'fail':
+                throw this.#error
+            case 'success':
+                return this.#data as T;
+        }
+    }
 }
