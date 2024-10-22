0292a35629dcc1117481029c21b97d39cad128e2
luiz <luizgfc@proton.me>
2024-10-22 00:57:12 -0300

    tools: display mocking of prompt event

--- control-panel/web/src/component/displayRenderer.tsx
+++ control-panel/web/src/component/displayRenderer.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useMemo, useState } from "react";
 import { Result } from "./result";
+import { Prompt } from "./prompt";
 
 type DisplayElement = {
     type: string;
@@ -14,12 +15,18 @@ type DisplayResult = {
     message: string;
 };
 
+type DisplayPrompt = {
+    title: string;
+    type: string;
+}
+
 type DisplayDefinitionType = 'result' | 'view' | 'prompt' | string
 
 export type DisplayDefinition = {
     type: DisplayDefinitionType;
     elements?: DisplayElement[];
     result?: DisplayResult;
+    prompt?: DisplayPrompt;
 };
 
 export type DsiplayRendererProps = {
@@ -27,7 +34,15 @@ export type DsiplayRendererProps = {
 }
 
 export function DisplayRenderer(props: DsiplayRendererProps) {
-    return (
-        <Result success={props.definition.result?.success ?? false}>{props.definition.result?.message ?? ''}</Result>
-    )
+    switch(props.definition.type) {
+        case 'result':
+            return (
+                <Result success={props.definition.result?.success ?? false}>{props.definition.result?.message ?? ''}</Result>
+            )
+        case 'prompt':
+            return (
+                <Prompt title={props.definition.prompt?.title ?? ''} onSubmit={(value) => console.log('prompt submit', value)}/>
+            )
+    }
+
 }
--- control-panel/web/src/page/toolViewExperiments.page.tsx
+++ control-panel/web/src/page/toolViewExperiments.page.tsx
@@ -16,6 +16,21 @@ import { useCallback, useMemo, useState } from "react"
 //     }
 // }
 
+const testEventPrompt = {
+    "class": "operation",
+    "type": "display",
+    "project": "proj-x",
+    "tool": "tool-y",
+    "display": {
+        "type": "prompt",
+        "prompt": {
+            "title": "Add some text:",
+            "type": "string",
+        }
+    }
+}
+
+
 type ToolEvent = {
     "class": string;
     "type": string;
@@ -25,43 +40,45 @@ type ToolEvent = {
 }
 
 export function ToolViewExperimentsPage() {
-    const [event, setEvent] = useState<ToolEvent | null>(null)
+    // const [event, setEvent] = useState<ToolEvent | null>(null)
 
-    console.log('e', event)
+    // console.log('e', event)
     
-    useMemo(() => {
-        const ws = new WebSocket(`${import.meta.env.PL_BACKEND_URL}/api/tool/client/ws`)
-        ws.addEventListener('open', () => {
-            console.log('socket open')
+    // useMemo(() => {
+    //     const ws = new WebSocket(`${import.meta.env.PL_BACKEND_URL}/api/tool/client/ws`)
+    //     ws.addEventListener('open', () => {
+    //         console.log('socket open')
 
-            ws.send(JSON.stringify({
-                "class": "interaction",
-                "type": "open",
-                "project": "proj-x",
-                "tool": "tool-b",
-            }))
-        })
+    //         ws.send(JSON.stringify({
+    //             "class": "interaction",
+    //             "type": "open",
+    //             "project": "proj-x",
+    //             "tool": "tool-b",
+    //         }))
+    //     })
 
-        ws.addEventListener('message', (e) => {
-            console.log(`recv: ${e.data}`)
-            setEvent(JSON.parse(e.data))
-        })
+    //     ws.addEventListener('message', (e) => {
+    //         console.log(`recv: ${e.data}`)
+    //         setEvent(JSON.parse(e.data))
+    //     })
 
-        return ws
-    }, [])
-    
+    //     return ws
+    // }, [])
+ 
+    const event = testEventPrompt
+
     if(!event) {
         return (
             <div className="bg-zinc-900 text-zinc-100 h-screen">
                     <div className="container mx-auto px-4 py-8">
                     <h1 className="text-3xl font-bold mb-6">Tool Sandbox</h1>
 
-                    <div className="flex justify-center">
+                    {/* <div className="flex justify-center">
                         <div className="w-4/5">
                             <Prompt title="Test prompt component" onSubmit={(value) => console.log('onsubmit', value)} />
                         </div>
                     </div>
-                    
+                     */}
                 </div>
             </div>
         )       
