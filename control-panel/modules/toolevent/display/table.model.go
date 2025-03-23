c4321d42777398cba71a68fc32bd8898e683b365
luizgfc <luizgfc@proton.me>
2025-03-23 09:51:37 -0300

    fix columns being reordered in table

--- control-panel/modules/toolevent/display/table.model.go
+++ control-panel/modules/toolevent/display/table.model.go
@@ -1,14 +1,13 @@
 package tooleventdisplay
 
-type TableColumnDictionary map[string]string
+import "encoding/json"
+
 type RowData map[string]string
 
-// FIXME: table column order is not deterministic, when reaching the go side
-//        it can change the order because it does not garantee the ordering of maps
 type TableDisplay struct {
-	Title   *string                `json:"title"`
-	Columns *TableColumnDictionary `json:"columns"`
-	Content []RowData              `json:"content"`
+	Title   *string         `json:"title"`
+	Columns json.RawMessage `json:"columns"`
+	Content []RowData       `json:"content"`
 }
 
 func (d *TableDisplay) IsValid() bool {
