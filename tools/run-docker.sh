c45ad04b800298c22720e68a8aa69be96ec0835d
luizgfc <luizgfc@proton.me>
2025-04-19 02:30:54 -0300

    fix run-docker script

--- tools/run-docker.sh
+++ tools/run-docker.sh
@@ -2,4 +2,4 @@
 docker build -t pl -f control-panel/Dockerfile .
 docker kill synthreon-dev
 docker rm synthreon-dev
-docker run --name synthreon-dev --env-file control-panel/demo.env --network=host -v "$PWD"/data:/data synt
+docker run --name synthreon-dev --env-file control-panel/demo.env --network=host -v "$PWD"/data:/data synthreon
