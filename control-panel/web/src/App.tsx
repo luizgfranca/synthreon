ab16a6697cd5b5f8c8eb63eda482108739d5d4c1
luizgfc <luizgfc@proton.me>
2025-04-20 23:23:23 -0300

    more trustworthy approach to redirect to authentication

--- control-panel/web/src/App.tsx
+++ control-panel/web/src/App.tsx
@@ -3,15 +3,23 @@ import "./index.css";
 import { router } from "./router";
 import { ContextProvider } from "./context/root";
 import { Suspense } from "react";
+import AuthService from "@/service/auth.service";
+
+const LOGIN_PATH = `${window.location.origin}${import.meta.env.PL_PATH_PREFIX}/login`;
 
 function App() {
-  window.document.documentElement.classList.add('dark')
+    window.document.documentElement.classList.add('dark')
 
-  return <ContextProvider>
-    <Suspense fallback={<h1>Loading...</h1>}>
-      <RouterProvider router={router} />
-    </Suspense>
-  </ContextProvider>;
+    if (!AuthService.isAuthenticated() && window.location.href !== LOGIN_PATH) {
+        window.location.href = LOGIN_PATH;
+        return <></>
+    }
+
+    return <ContextProvider>
+        <Suspense fallback={<h1>Loading...</h1>}>
+            <RouterProvider router={router} />
+        </Suspense>
+    </ContextProvider>;
 }
 
 export default App;
--- control-panel/web/src/page/projectSelector.page.tsx
+++ control-panel/web/src/page/projectSelector.page.tsx
@@ -4,27 +4,28 @@ import { Suspense } from "react";
 import { useNavigate } from "react-router-dom";
 
 export function ProjectSelector() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  if(!AuthService.isAuthenticated()) {
-    // TODO: create navigator that already computes prefix path
-    navigate(`${import.meta.env.PL_PATH_PREFIX}/login`)
-  }
+    console.log('ProjectSelector');
 
+    if (!AuthService.isAuthenticated()) {
+        // TODO: create navigator that already computes prefix path
+        navigate(`${import.meta.env.PL_PATH_PREFIX}/login`)
+    }
 
-  return (
-    <div className="bg-zinc-900 text-zinc-100 h-screen">
-      <div className="container mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold mb-6">Projects</h1>
+    return (
+        <div className="bg-zinc-900 text-zinc-100 h-screen">
+            <div className="container mx-auto px-4 py-8">
+                <h1 className="text-3xl font-bold mb-6">Projects</h1>
 
-        <Suspense
-          fallback={<h2 className="text-xl font-bold mb-6">Loading...</h2>}
-        >
-          <div className="space-y-4">
-            <ProjectList />
-          </div>
-        </Suspense>
-      </div>
-    </div>
-  );
+                <Suspense
+                    fallback={<h2 className="text-xl font-bold mb-6">Loading...</h2>}
+                >
+                    <div className="space-y-4">
+                        <ProjectList />
+                    </div>
+                </Suspense>
+            </div>
+        </div>
+    );
 }
