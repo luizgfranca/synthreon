0bf3cc08ad3f03de11c48165336f1c897b09824a
luizgfc <luizgfc@proton.me>
2025-04-27 21:16:19 -0300

    fix: dynamically created tool not appearing rightt on next reload

--- control-panel/web/src/datasource/tool.datasource.ts
+++ control-panel/web/src/datasource/tool.datasource.ts
@@ -4,13 +4,18 @@ import { Datasource } from "suspense-datasource";
 
 export class ToolDatasource extends Datasource<ToolDto[]> {
     async fetch(projectAcronym: string): Promise<ToolDto[]> {
-        return await ToolService.queryProjectTools(projectAcronym)        
+        // FIXME: temporary, disabling the cache until better implementation
+        // that will invalidate the cache when:
+        //  - a certain time has passed
+        //  - there was a timeout
+        this.reset();
+        return await ToolService.queryProjectTools(projectAcronym);
     }
 
     async create(projectAcronym: string, data: NewToolDto) {
         // TODO: should add some logical validaions here
-        await ToolService.createTool(projectAcronym, data)
-        this.reset()
+        await ToolService.createTool(projectAcronym, data);
+        this.reset();
     }
 
 }
